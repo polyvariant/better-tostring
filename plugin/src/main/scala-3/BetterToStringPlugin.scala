8aa1f4f082cf5cd9dac61053cd019ccc532df0dc
Jakub Kozłowski <kubukoz@gmail.com>
2021-04-13 23:28:21 +0200

    POC: bm4 removing final `map`

--- plugin/src/main/scala-3/BetterToStringPlugin.scala
+++ plugin/src/main/scala-3/BetterToStringPlugin.scala
@@ -3,6 +3,8 @@ package com.kubukoz
 import dotty.tools.dotc.ast.tpd
 import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Flags.Module
+import dotty.tools.dotc.core.Names.*
+import dotty.tools.dotc.core.Names
 import dotty.tools.dotc.plugins.{PluginPhase, StandardPlugin}
 import dotty.tools.dotc.typer.FrontEnd
 import tpd._
@@ -16,6 +18,58 @@ final class BetterToStringPluginPhase extends PluginPhase:
 
   override val phaseName: String = "better-tostring-phase"
   override val runsAfter: Set[String] = Set(FrontEnd.name)
+
+  override def transformApply(apply: Apply)(using ctx: Context): Tree = {
+    var selector: Tree = null
+    val matches = apply.filterSubTrees {
+      case appl@Apply(function, List(param)) =>
+
+        function.filterSubTrees {
+          case Select(selected, name) if name.show == "map"  =>
+            param match {
+              case Block(
+                List(
+                  DefDef(
+                    name,
+                    List(
+                      List(
+                        ValDef(
+                          term,
+                          tpe1,
+                          EmptyTree
+                        )
+                      )
+                    ),
+                    tpe2,
+                    Ident(term2)
+                  )
+                ),
+                Closure(
+                  Nil,
+                  Ident(name2),
+                  EmptyTree
+                )
+              )
+                if name == name2 && term == term2 && tpe1.sameTree(tpe2)
+              =>
+                selector = selected
+
+                true
+            }
+          case _ => false
+        }.nonEmpty
+
+      case _ => false
+    }.nonEmpty
+
+    // if(matches)
+    //   println(selector.show)
+
+    if(matches)
+      selector
+    else
+      super.transformApply(apply)
+  }
 
   override def transformTemplate(t: Template)(using ctx: Context): Tree =
     val clazz = ctx.owner.asClass
--- tests/src/test/scala/Demo.scala
+++ tests/src/test/scala/Demo.scala
@@ -1,74 +1,18 @@
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class Tests extends AnyWordSpec with Matchers {
-
-  "Simple case class" should {
-    "stringify nicely" in {
-      SimpleCaseClass(
-        "Joe",
-        23
-      ).toString shouldBe "SimpleCaseClass(name = Joe, age = 23)"
-
-    }
-  }
-
-  "Case class with multiple parameter lists" should {
-    "only have the first list included" in {
-      MultiParameterList("foo", 20)(
-        "s"
-      ).toString shouldBe "MultiParameterList(name = foo, age = 20)"
-    }
-  }
-
-  "Case class with custom toString" should {
-    "use it" in {
-      CustomTostring("Joe").toString shouldBe "***"
-    }
-  }
-
-  "Method with alternate constructors" should {
-    "stringify based on primary constructor" in {
-      new HasOtherConstructors(
-        10
-      ).toString shouldBe "HasOtherConstructors(s = 10 beers)"
-    }
-  }
-
-  "Class nested in another class" should {
-    "stringify normally" in {
-      new NestedParent().NestedChild("a").toString shouldBe "NestedChild(a)"
-    }
-  }
-
-  "Method-local class" should {
-    "stringify normally" in {
-      MethodLocalWrapper.methodLocalClassStringify shouldBe "LocalClass(a)"
-    }
-  }
+trait IO[A] {
+  def flatMap[B](f: A => IO[B]): IO[B]
+  def map[B](f: A => B): IO[B]
 }
 
-final case class SimpleCaseClass(name: String, age: Int)
-final case class MultiParameterList(name: String, age: Int)(val s: String)
+object Demo {
 
-final case class CustomTostring(name: String) {
-  override def toString: String = "***"
-}
-
-final case class HasOtherConstructors(s: String) {
-  def this(a: Int) = this(a.toString + " beers")
-}
-
-final class NestedParent() {
-  case class NestedChild(name: String)
-}
-
-object MethodLocalWrapper {
-
-  def methodLocalClassStringify: String = {
-    final case class LocalClass(name: String)
-
-    LocalClass("a").toString()
+  def foo[A](ioa: IO[A]): IO[Int] = com.kubukoz.DebugUtils.withDesugar {
+    for {
+      a <- ioa
+      x <- foo(ioa)
+    } yield x
   }
 
 }
